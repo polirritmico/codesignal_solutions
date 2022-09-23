c4b26e411b97a9f7ce433b5ddf5ebb905e917207
polirritmico <ebray187@gmail.com>
2022-09-22 22:20:47 -0300

    Added solution to question 48

---
+++ Python/isDigit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def solution(char):
+    return char.isdecimal()
+
+
+def main():
+    case = "a"
+    print(solution(case))
+
+if __name__ == "__main__":
+    main()
+
