d2866f6135de8879e011da4d21bea0bf00943860
polirritmico <ebray187@gmail.com>
2023-09-06 11:38:46 -0300

    WIP

--- numberOfClans.py
+++ numberOfClans.py
@@ -18,14 +18,36 @@ numbers 1 through 6 are broken into 4 clans.
 """
 
 
-def solution(divisors, k):
-    pass
+def is_friendly(number: int, divisors: list[int]) -> bool:
+    prev_divisible = None
+    for divisor in divisors:
+        is_divisible = number % divisor == 0
+        prev_divisible = is_divisible if prev_divisible is None else prev_divisible
+        if is_divisible != prev_divisible:
+            return False
+    return True
+
+
+def solution(divisors: list[int], k: int) -> int:
+    integers_to_check = [integer for integer in range(1, k)]
+    friendly_numbers = []
+    for idx_a in range(0, len(integers_to_check) - 1):
+        num_a = integers_to_check[idx_a]
+        if not is_friendly(num_a, divisors):
+            continue
+        for idx_b in range(idx_a + 1, len(integers_to_check)):
+            num_b = integers_to_check[idx_b]
+            if not is_friendly(num_b, divisors):
+                continue
+            friends = (num_a, num_b)
+            friendly_numbers.append(friends)
+    return friendly_numbers
 
 
 def main():
-    divisors = [2, 3]
+    divisors = [2, 3, 4]
     k = 6
-    # expected = 4
+    # expected = 5
     print(solution(divisors, k))
 
 
