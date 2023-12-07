13b8c98e38c80ed03239344eac3082eb598e543b
polirritmico <ebray187@gmail.com>
2023-12-07 17:24:30 -0300

    Update templates from unittest to pytest

---
+++ Python/template_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from REPLACE import solution
+
+
+# @pytest.mark.skip()
+def test_case1():
+    case = 
+    expected = 
+    output = solution(case)
+    assert output == expected
